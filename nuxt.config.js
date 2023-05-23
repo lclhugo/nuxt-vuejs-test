70d05103c4e3e9b7b24c68c642074ef18c621181
lclhugo <hugo.laclau@gmail.com>
2023-05-24 01:38:10 +0200

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: false,
-  router: {
-    base: '/pokedex/'
-  }
-  ,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'pokedex',
